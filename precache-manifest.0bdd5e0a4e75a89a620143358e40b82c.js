00e4b9a57390e93eb3521f4653e72991e824ea3b
nmr5651 <nmr5651@rit.edu>
2020-07-14 14:39:21 -0400

    Updates

---
+++ precache-manifest.0bdd5e0a4e75a89a620143358e40b82c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "226186aa11d2ac9e84f59fe34cf03bdd",
+    "url": "/UPS_Sorting/index.html"
+  },
+  {
+    "revision": "720c3b07e3a2b56d4b0c",
+    "url": "/UPS_Sorting/static/css/main.29ca2c6b.chunk.css"
+  },
+  {
+    "revision": "c1a9f3bc10f322161dcf",
+    "url": "/UPS_Sorting/static/js/2.1722903b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/UPS_Sorting/static/js/2.1722903b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cce1ef6c8666702aecce",
+    "url": "/UPS_Sorting/static/js/3.d7331f45.chunk.js"
+  },
+  {
+    "revision": "287e48aeac3564a8b2b5685a67fb9216",
+    "url": "/UPS_Sorting/static/js/3.d7331f45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "720c3b07e3a2b56d4b0c",
+    "url": "/UPS_Sorting/static/js/main.0a47bf6c.chunk.js"
+  },
+  {
+    "revision": "9a33ea947c03873b368f",
+    "url": "/UPS_Sorting/static/js/runtime-main.f7ff803f.js"
+  },
+  {
+    "revision": "8e3d6124f258a1690567f02ade38cd2d",
+    "url": "/UPS_Sorting/static/media/corrrect.8e3d6124.mp3"
+  },
+  {
+    "revision": "cbf956ffcae87c0d84352daebf8e8391",
+    "url": "/UPS_Sorting/static/media/labelside.cbf956ff.png"
+  },
+  {
+    "revision": "881843a7135b8abd4794bd2d33dc8b3c",
+    "url": "/UPS_Sorting/static/media/upslogo.881843a7.png"
+  },
+  {
+    "revision": "8c5e329845615176bb1b8218878e7e5c",
+    "url": "/UPS_Sorting/static/media/wrong.8c5e3298.mp3"
+  }
+]);
